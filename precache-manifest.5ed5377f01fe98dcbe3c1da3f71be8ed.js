1e4531d31bc4a50d75d3927aca5b48987727ed98
Puppywhore <puppywhoreforfenris@gmail.com>
2019-07-10 00:53:41 +0200

    Deploy

---
+++ precache-manifest.5ed5377f01fe98dcbe3c1da3f71be8ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1272c1d312a91c05b78fd27c799e54f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5752a12cfc645543730",
+    "url": "/static/css/2.08c8ee57.chunk.css"
+  },
+  {
+    "revision": "884b9214cd59c42428a1",
+    "url": "/static/css/main.eb2aad1b.chunk.css"
+  },
+  {
+    "revision": "f5752a12cfc645543730",
+    "url": "/static/js/2.258f12ae.chunk.js"
+  },
+  {
+    "revision": "884b9214cd59c42428a1",
+    "url": "/static/js/main.d836975f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8644808f9829dd431e1b7242da8c1506",
+    "url": "/static/media/bed.8644808f.jpg"
+  },
+  {
+    "revision": "5d56de0cc1c074c9e7127dd8dbb6d3fa",
+    "url": "/static/media/kathrine.5d56de0c.jpg"
+  },
+  {
+    "revision": "365dd511ad0facfe1c2b0b960f9b72ea",
+    "url": "/static/media/pen.365dd511.jpg"
+  }
+]);
--- src/redux/reducers/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import {choiceReducer} from "./choiceReducer";
-import {timeReducer} from "./timeReducer"
-
-export default combineReducers({choiceReducer,timeReducer})
